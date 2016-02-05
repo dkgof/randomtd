e41e489e1ee9ae4fd145a2c035d859404e220cca
rolf <rolf@10.100.23.27>
2016-02-05 13:26:34 +0100

    More ui stuff

---
+++ src/main/java/dk/lystrup/randomtd/ui/GamePanel.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.lystrup.randomtd.ui;
+
+import dk.lystrup.randomtd.domain.Entity;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author rolf
+ */
+public class GamePanel extends JPanel {
+
+    private static GamePanel instance;
+    
+    public static GamePanel instance() {
+        if(instance == null) {
+            instance = new GamePanel();
+        }
+        
+        return instance;
+    }
+    
+    private final List<Entity> entities;
+    
+    private GamePanel() {
+        entities = new ArrayList<>();
+    }
+    
+    @Override
+    public void paintComponents(Graphics g) {
+        super.paintComponents(g);
+        
+        Graphics2D g2 = (Graphics2D) g;
+        
+        synchronized(entities) {
+            entities.parallelStream().forEach((entity) -> {
+                entity.draw(g2);
+            });
+        }
+    }    
+
+    public void addEntity(Entity e) {
+        synchronized(entities) {
+            entities.add(e);
+        }
+    }
+    
+    public void removeEntity(Entity e) {
+        synchronized(entities) {
+            entities.remove(e);
+        }
+    }
+}
