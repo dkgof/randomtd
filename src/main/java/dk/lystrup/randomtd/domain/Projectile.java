f9e82a735ffe02e8d1ce99e86237157ffe830224
Thor <Thor@10.100.22.79>
2016-02-05 13:01:24 +0100

    added damage types

--- src/main/java/dk/lystrup/randomtd/domain/Projectile.java
+++ src/main/java/dk/lystrup/randomtd/domain/Projectile.java
@@ -5,10 +5,44 @@
  */
 package dk.lystrup.randomtd.domain;
 
+import dk.lystrup.randomtd.domain.NPC.ArmorType;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author rolf
  */
-public abstract class Projectile extends Entity{
+public abstract class Projectile extends Entity {
+
+    public enum DamageType {
+
+        //(none, light, medium, heavy)
+        PHYSICAL(generateDamageMap(1.1, 1.0, 0.9, 0.8)),
+        MAGICAL(generateDamageMap(1, 0.8, 0.9, 1.1));
+
+        DamageType(Map<ArmorType, Double> dmgMap) {
+            damageMultipliers = dmgMap;
+        }
+
+        private final Map<ArmorType, Double> damageMultipliers;
+
+        private static Map<ArmorType, Double> generateDamageMap(double... multipliers) {
+            Map<ArmorType, Double> dmgMap = new HashMap<>();
+
+            int i = 0;
+            for(ArmorType armorType : ArmorType.values()){
+                dmgMap.put(armorType, multipliers[i]);
+                i++;    
+            }
+            
+            return dmgMap;
+        }
+
+        public double getDamageVersus(ArmorType armor) {
+            return damageMultipliers.get(armor);
+        }
+    }
     
+    private Entity target;
 }
