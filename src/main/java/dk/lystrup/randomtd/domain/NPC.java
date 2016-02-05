38ccd442567ce6bc7c656987867e924fd096b2da
rolf <rolf@10.100.23.27>
2016-02-05 12:28:33 +0100

    Initial commit

---
+++ src/main/java/dk/lystrup/randomtd/domain/NPC.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.lystrup.randomtd.domain;
+
+/**
+ *
+ * @author rolf
+ */
+public abstract class NPC {
+    
+    public enum ArmorType {
+        NONE,
+        LIGHT,
+        MEDIUM,
+        HEAVY
+    }
+    
+    private String name;
+    
+    private double maxHealth;
+    
+    private double currentHealth;
+    
+    private double armor;
+    
+    private ArmorType armorType;
+    
+    private double movementSpeed;
+    
+    private boolean flying;
+}
